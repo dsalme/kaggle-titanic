9613933e582e43b1fcf3484b2a8504f3c8c1ff35
David Salmeron <david@brainhi.com>
2024-04-01 19:59:26 +0200

    added readme and minor tweaks

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-lalala
+## Simple Flask + Vue app that serves the [Titanic ML competition Model](https://risx3.github.io/titanic-analysis/)
+### Submit your predictions
+
+It was trained by following [this guide](https://risx3.github.io/titanic-analysis/)
+
+### Endpoints
+- `/`
+It's the root url, which returns a frontend view
+- `/submit_prediction`
+Lets you submit a csv file for running model predictions.
+The csv file should have the following columns:
+`Age,SibSp,Parch,Fare,male,Q,S,2,3`
+And should have exactly 268 (plus 1 for the headers, so 269) rows.
+
+### How to run it
+The app is composed by one docker container, to run it, run this in your console after cloning it:
+`docker-compose up --build`
+
+### How its structured
+The app consists in a Flask App, which serves a web app through the `/` route.
+The frontend app is a simple index.html file which loads VueJS through ES modules, and has a VueJS form component for handling the csv submission and showing the `/submit_prediction`'s endpoint response.
--- flask_app/app.py
+++ flask_app/app.py
@@ -1,3 +1,4 @@
+from typing import Any, Dict
 import numpy as np
 import pandas as pd
 
@@ -9,7 +10,9 @@ import logging
 
 from flask import Flask, render_template, request, jsonify
 
-app = Flask(__name__)
+STATIC_FOLDER = 'templates/assets'
+app = Flask(__name__, static_folder=STATIC_FOLDER)
+
 
 if __name__ != '__main__':
     gunicorn_logger = logging.getLogger('gunicorn.error')
@@ -17,19 +20,19 @@ if __name__ != '__main__':
     app.logger.setLevel(gunicorn_logger.level)
 
 
-train = pd.read_csv('/src/kaggle/input/titanic/train.csv')
-test = pd.read_csv('/src/kaggle/input/titanic/test.csv')
+train: pd.DataFrame = pd.read_csv('/src/kaggle/input/titanic/train.csv')
+test: pd.DataFrame = pd.read_csv('/src/kaggle/input/titanic/test.csv')
 
 # train
-sex = pd.get_dummies(train['Sex'], drop_first = True)
-embark = pd.get_dummies(train['Embarked'],drop_first=True)
-pcl = pd.get_dummies(train['Pclass'],drop_first=True)
+sex: pd.DataFrame = pd.get_dummies(train['Sex'], drop_first = True)
+embark: pd.DataFrame = pd.get_dummies(train['Embarked'],drop_first=True)
+pcl: pd.DataFrame = pd.get_dummies(train['Pclass'],drop_first=True)
 
 train = pd.concat([train,sex,embark,pcl],axis=1)
 
 train.drop(['Pclass','Sex','Embarked','Cabin','PassengerId','Name','Ticket'],axis=1, inplace=True)
 
-train_values = {'Age': round(np.mean(train['Age']))}
+train_values: Dict[str, Any] = {'Age': round(np.mean(train['Age']))}
 train = train.fillna(value = train_values)
 
 # test
@@ -41,7 +44,7 @@ test = pd.concat([test,sex,embark,pcl],axis=1)
 
 test.drop(['Pclass','Sex','Embarked','Cabin','PassengerId','Name','Ticket'],axis=1, inplace=True)
 
-test_values = {'Age':round(np.mean(test['Age'])), 'Fare':round(np.mean(test['Fare']))}
+test_values: Dict[str, Any] = {'Age':round(np.mean(test['Age'])), 'Fare':round(np.mean(test['Fare']))}
 test = test.fillna(value = test_values)
 
 X = train.drop('Survived',axis=1)
@@ -49,8 +52,8 @@ X.columns = X.columns.astype(str)
 y = train['Survived']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
-output_vars = X_test
-output_vars.to_csv('test_vars.csv', index=False)
+#output_vars = X_test
+#output_vars.to_csv('test_vars.csv', index=False)
 
 logmodel = LogisticRegression(solver = 'liblinear')
 
@@ -66,7 +69,7 @@ app.logger.info(f"my score: {my_score}")
 logmodel = joblib.load("modelo_titanic.pkl")
 
 @app.route('/submit_variables', methods=['POST'])
-def submit_predict():
+def submit_predict() -> Dict[str, Any]:
     if 'uploaded_file' not in request.files:
         return jsonify({
             "error":f'No upload_file provided for the submit_variables endpoint'
@@ -75,18 +78,28 @@ def submit_predict():
     uploaded_file = request.files['uploaded_file']
     if uploaded_file.filename == '':
         return jsonify({"error": "uploaded_file is invalid"}), 400
+    
+    app.logger.info(f'filename: {uploaded_file.filename}')
+    
+    if not uploaded_file.filename.lower().endswith('.csv'):
+        return jsonify({"error": "uploaded_file has invalid extension, please upload a csv file"}), 400
 
     if uploaded_file:
-        test_vars_df = pd.read_csv(uploaded_file)
-        uploaded_predict = logmodel.predict(test_vars_df)
-        accuracy = accuracy_score(y_test, uploaded_predict)
-        app.logger.info(f"uploaded file accuracy test: {accuracy}")
-        return jsonify({
-            "data":{
-                "predictions": uploaded_predict.tolist(),
-                "accuracy": f'{accuracy}'
-            }
-        })
+        try:
+            test_vars_df = pd.read_csv(uploaded_file)
+            uploaded_predict = logmodel.predict(test_vars_df.iloc[:1])
+            accuracy = accuracy_score(y_test, uploaded_predict)
+            app.logger.info(f"uploaded file accuracy test: {accuracy}")
+            return jsonify({
+                "data":{
+                    "predictions": uploaded_predict.tolist(),
+                    "accuracy": f'{accuracy}'
+                }
+            })
+        except Exception as e:
+            return jsonify({
+                "error":f'error on submit_variables: {e}'
+            }), 400
 
 @app.route('/')
 def index():
